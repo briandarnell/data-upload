e2f5dfe32a716d2506cc77d42f0c84a39eadf6bc
Brian Darnell <mrbriandarnell@gmail.com>
2024-07-18 09:22:57 +0100

    Root redirect permanent=False

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="data/", permanent=True)),
+    path("", RedirectView.as_view(url="data/", permanent=False)),
     path("admin/", admin.site.urls),
     path("data/", include("data.urls")),
 ]
